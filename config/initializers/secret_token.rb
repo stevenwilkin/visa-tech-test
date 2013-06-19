bd615239fee88a8c689a96f8c35986305cb77983
Steven Wilkin <stevenwilkin@gmail.com>
2013-06-19 22:42:55 +0100

    freshly generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisaTechTest::Application.config.secret_token = 'f9d51d20a8752cda87023d0fd64798d9b217047b90590219e66c24ed389c47b27c8da0083c3602958ad6e2c114668510c4e0a3319d9a249769e9a12c598e77eb'
